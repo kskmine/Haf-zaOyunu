5311653591f632d2bdd22c7264ab21aedd03871e
Emine <emine.kosak8@gmail.com>
2021-04-13 19:10:08 +0300

    Initial commit

---
+++ HafızaOyunu/Form1.Designer.cs
@@ -0,0 +1,302 @@
+﻿namespace HafızaOyunu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox41 = new System.Windows.Forms.PictureBox();
+            this.pictureBox42 = new System.Windows.Forms.PictureBox();
+            this.pictureBox43 = new System.Windows.Forms.PictureBox();
+            this.pictureBox44 = new System.Windows.Forms.PictureBox();
+            this.pictureBox45 = new System.Windows.Forms.PictureBox();
+            this.pictureBox46 = new System.Windows.Forms.PictureBox();
+            this.pictureBox47 = new System.Windows.Forms.PictureBox();
+            this.pictureBox48 = new System.Windows.Forms.PictureBox();
+            this.pictureBox49 = new System.Windows.Forms.PictureBox();
+            this.pictureBox510 = new System.Windows.Forms.PictureBox();
+            this.pictureBox511 = new System.Windows.Forms.PictureBox();
+            this.pictureBox512 = new System.Windows.Forms.PictureBox();
+            this.pictureBox513 = new System.Windows.Forms.PictureBox();
+            this.pictureBox514 = new System.Windows.Forms.PictureBox();
+            this.pictureBox515 = new System.Windows.Forms.PictureBox();
+            this.pictureBox516 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox41)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox42)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox43)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox44)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox45)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox46)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox47)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox48)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox49)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox510)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox511)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox512)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox513)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox514)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox515)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox516)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox41
+            // 
+            this.pictureBox41.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pictureBox41.Location = new System.Drawing.Point(22, 29);
+            this.pictureBox41.Name = "pictureBox41";
+            this.pictureBox41.Size = new System.Drawing.Size(89, 88);
+            this.pictureBox41.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox41.TabIndex = 0;
+            this.pictureBox41.TabStop = false;
+            this.pictureBox41.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox42
+            // 
+            this.pictureBox42.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pictureBox42.Location = new System.Drawing.Point(139, 29);
+            this.pictureBox42.Name = "pictureBox42";
+            this.pictureBox42.Size = new System.Drawing.Size(89, 88);
+            this.pictureBox42.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox42.TabIndex = 1;
+            this.pictureBox42.TabStop = false;
+            this.pictureBox42.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox43
+            // 
+            this.pictureBox43.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pictureBox43.Location = new System.Drawing.Point(253, 29);
+            this.pictureBox43.Name = "pictureBox43";
+            this.pictureBox43.Size = new System.Drawing.Size(89, 88);
+            this.pictureBox43.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox43.TabIndex = 2;
+            this.pictureBox43.TabStop = false;
+            this.pictureBox43.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox44
+            // 
+            this.pictureBox44.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pictureBox44.Location = new System.Drawing.Point(367, 29);
+            this.pictureBox44.Name = "pictureBox44";
+            this.pictureBox44.Size = new System.Drawing.Size(89, 88);
+            this.pictureBox44.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox44.TabIndex = 3;
+            this.pictureBox44.TabStop = false;
+            this.pictureBox44.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox45
+            // 
+            this.pictureBox45.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pictureBox45.Location = new System.Drawing.Point(22, 152);
+            this.pictureBox45.Name = "pictureBox45";
+            this.pictureBox45.Size = new System.Drawing.Size(89, 88);
+            this.pictureBox45.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox45.TabIndex = 4;
+            this.pictureBox45.TabStop = false;
+            this.pictureBox45.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox46
+            // 
+            this.pictureBox46.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pictureBox46.Location = new System.Drawing.Point(139, 152);
+            this.pictureBox46.Name = "pictureBox46";
+            this.pictureBox46.Size = new System.Drawing.Size(89, 88);
+            this.pictureBox46.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox46.TabIndex = 5;
+            this.pictureBox46.TabStop = false;
+            this.pictureBox46.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox47
+            // 
+            this.pictureBox47.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pictureBox47.Location = new System.Drawing.Point(253, 152);
+            this.pictureBox47.Name = "pictureBox47";
+            this.pictureBox47.Size = new System.Drawing.Size(89, 88);
+            this.pictureBox47.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox47.TabIndex = 6;
+            this.pictureBox47.TabStop = false;
+            this.pictureBox47.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox48
+            // 
+            this.pictureBox48.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pictureBox48.Location = new System.Drawing.Point(367, 152);
+            this.pictureBox48.Name = "pictureBox48";
+            this.pictureBox48.Size = new System.Drawing.Size(89, 88);
+            this.pictureBox48.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox48.TabIndex = 7;
+            this.pictureBox48.TabStop = false;
+            this.pictureBox48.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox49
+            // 
+            this.pictureBox49.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pictureBox49.Location = new System.Drawing.Point(22, 269);
+            this.pictureBox49.Name = "pictureBox49";
+            this.pictureBox49.Size = new System.Drawing.Size(89, 88);
+            this.pictureBox49.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox49.TabIndex = 8;
+            this.pictureBox49.TabStop = false;
+            this.pictureBox49.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox510
+            // 
+            this.pictureBox510.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pictureBox510.Location = new System.Drawing.Point(139, 269);
+            this.pictureBox510.Name = "pictureBox510";
+            this.pictureBox510.Size = new System.Drawing.Size(89, 88);
+            this.pictureBox510.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox510.TabIndex = 9;
+            this.pictureBox510.TabStop = false;
+            this.pictureBox510.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox511
+            // 
+            this.pictureBox511.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pictureBox511.Location = new System.Drawing.Point(253, 269);
+            this.pictureBox511.Name = "pictureBox511";
+            this.pictureBox511.Size = new System.Drawing.Size(89, 88);
+            this.pictureBox511.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox511.TabIndex = 10;
+            this.pictureBox511.TabStop = false;
+            this.pictureBox511.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox512
+            // 
+            this.pictureBox512.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pictureBox512.Location = new System.Drawing.Point(367, 269);
+            this.pictureBox512.Name = "pictureBox512";
+            this.pictureBox512.Size = new System.Drawing.Size(89, 88);
+            this.pictureBox512.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox512.TabIndex = 11;
+            this.pictureBox512.TabStop = false;
+            this.pictureBox512.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox513
+            // 
+            this.pictureBox513.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pictureBox513.Location = new System.Drawing.Point(22, 378);
+            this.pictureBox513.Name = "pictureBox513";
+            this.pictureBox513.Size = new System.Drawing.Size(89, 88);
+            this.pictureBox513.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox513.TabIndex = 12;
+            this.pictureBox513.TabStop = false;
+            this.pictureBox513.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox514
+            // 
+            this.pictureBox514.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pictureBox514.Location = new System.Drawing.Point(139, 378);
+            this.pictureBox514.Name = "pictureBox514";
+            this.pictureBox514.Size = new System.Drawing.Size(89, 88);
+            this.pictureBox514.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox514.TabIndex = 13;
+            this.pictureBox514.TabStop = false;
+            this.pictureBox514.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox515
+            // 
+            this.pictureBox515.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pictureBox515.Location = new System.Drawing.Point(253, 378);
+            this.pictureBox515.Name = "pictureBox515";
+            this.pictureBox515.Size = new System.Drawing.Size(89, 88);
+            this.pictureBox515.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox515.TabIndex = 14;
+            this.pictureBox515.TabStop = false;
+            this.pictureBox515.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox516
+            // 
+            this.pictureBox516.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pictureBox516.Location = new System.Drawing.Point(367, 378);
+            this.pictureBox516.Name = "pictureBox516";
+            this.pictureBox516.Size = new System.Drawing.Size(89, 88);
+            this.pictureBox516.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox516.TabIndex = 15;
+            this.pictureBox516.TabStop = false;
+            this.pictureBox516.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(514, 492);
+            this.Controls.Add(this.pictureBox516);
+            this.Controls.Add(this.pictureBox515);
+            this.Controls.Add(this.pictureBox514);
+            this.Controls.Add(this.pictureBox513);
+            this.Controls.Add(this.pictureBox512);
+            this.Controls.Add(this.pictureBox511);
+            this.Controls.Add(this.pictureBox510);
+            this.Controls.Add(this.pictureBox49);
+            this.Controls.Add(this.pictureBox48);
+            this.Controls.Add(this.pictureBox47);
+            this.Controls.Add(this.pictureBox46);
+            this.Controls.Add(this.pictureBox45);
+            this.Controls.Add(this.pictureBox44);
+            this.Controls.Add(this.pictureBox43);
+            this.Controls.Add(this.pictureBox42);
+            this.Controls.Add(this.pictureBox41);
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox41)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox42)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox43)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox44)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox45)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox46)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox47)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox48)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox49)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox510)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox511)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox512)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox513)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox514)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox515)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox516)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+       
+        private System.Windows.Forms.PictureBox pictureBox41;
+        private System.Windows.Forms.PictureBox pictureBox42;
+        private System.Windows.Forms.PictureBox pictureBox43;
+        private System.Windows.Forms.PictureBox pictureBox44;
+        private System.Windows.Forms.PictureBox pictureBox45;
+        private System.Windows.Forms.PictureBox pictureBox46;
+        private System.Windows.Forms.PictureBox pictureBox47;
+        private System.Windows.Forms.PictureBox pictureBox48;
+        private System.Windows.Forms.PictureBox pictureBox49;
+        private System.Windows.Forms.PictureBox pictureBox510;
+        private System.Windows.Forms.PictureBox pictureBox511;
+        private System.Windows.Forms.PictureBox pictureBox512;
+        private System.Windows.Forms.PictureBox pictureBox513;
+        private System.Windows.Forms.PictureBox pictureBox514;
+        private System.Windows.Forms.PictureBox pictureBox515;
+        private System.Windows.Forms.PictureBox pictureBox516;
+    }
+}
+
---
+++ HafızaOyunu/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HafızaOyunu
+{
+    public partial class Form1 : Form
+    {
+        Image[] resimler = {
+                 Properties.Resources.arımaya,
+                 Properties.Resources.casper,
+                 Properties.Resources.garfield,
+                 Properties.Resources.jerry,
+                 Properties.Resources.mickymouse,
+                 Properties.Resources.pembepanter,
+                 Properties.Resources.scobydoo,
+                 Properties.Resources.temelreis
+
+            };
+        int[] indeksler = {0,0,1,1,2,2,3,3,4,4,5,5,6,6,7,7};
+
+        PictureBox ilkKutu;
+        int ilkIndeks,bulunan,deneme;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            resimleriKaristir();
+        }
+
+        private void resimleriKaristir()
+        {
+           
+            Random rnd = new Random();
+            for (int i = 0; i <15 ; i++)
+            {
+                int sayi = rnd.Next(15);
+                int gecici = indeksler[i];
+                indeksler[i] = indeksler[sayi];
+                indeksler[sayi] = gecici;
+            }
+        }
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            PictureBox kutu=(PictureBox)sender;
+            int kutuNo = int.Parse(kutu.Name.Substring(11));
+            int indeksNo = indeksler[kutuNo-1];
+            kutu.Image = resimler[indeksNo];
+
+            kutu.Refresh();
+
+            if (ilkKutu==null)
+            {
+                ilkKutu = kutu;
+                ilkIndeks = indeksNo;
+                deneme++;
+            }
+            else
+            {
+                System.Threading.Thread.Sleep(1000);
+
+                ilkKutu.Image = null;
+                kutu.Image = null;
+
+                if (ilkIndeks==indeksNo)
+                {
+                    bulunan++;
+                    ilkKutu.Visible = false;
+                    kutu.Visible = false;
+
+                    if (bulunan==8)
+                    {
+                        MessageBox.Show("Tebrikler"+deneme+"denemede buldunuz");
+                        bulunan = 0;
+                        deneme = 0;
+
+                        foreach (Control kontrol in Controls)
+                        {
+                            kontrol.Visible = true;
+                        }
+                        resimleriKaristir();
+                    }
+                }
+                ilkKutu = null;
+            }
+        }
+    }
+}
